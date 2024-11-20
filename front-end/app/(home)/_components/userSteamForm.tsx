b388a918d30ab1a54e25b06d7be8bb373ab96ecb
Arthur Alexi <90854173+ArthurAlexi@users.noreply.github.com>
2024-11-20 07:58:43 -0300

    style: trocando textos para ingles

--- front-end/app/(home)/_components/userSteamForm.tsx
+++ front-end/app/(home)/_components/userSteamForm.tsx
@@ -15,7 +15,7 @@ export default function UserSteamForm() {
         const data = new FormData(event.currentTarget)
         const user = data.get('user')?.toString()
         
-        if(!user) return alert('Informe um nome.')
+        if(!user) return alert('Please, inform a account.')
 
         router.push(`/${user}`)
     }
@@ -26,7 +26,7 @@ export default function UserSteamForm() {
                 <form className="flex gap-5 flex-wrap justify-center" onSubmit={handleSubmit}>
                     <div className="flex-1 flex items-center gap-3">
                         <User className="text-violet-500" size={24} />
-                        <input type="text" placeholder="Usuário da Steam" name="user"
+                        <input type="text" placeholder="Steam Account" name="user"
                             className=" flex-1 bg-transparent text-lg placeholder:text-zinc-400 outline-none bg-none focus:border-b pb-1" />
 
                     </div>
@@ -34,13 +34,17 @@ export default function UserSteamForm() {
                     <div className="w-full h-px bg-zinc-700 sm:w-px sm:h-10" />
 
                     <button type="submit" className="font-medium rounded-lg py-2 px-5 flex justify-center items-center gap-2 bg-violet-600 text-violet-200 w-full sm:w-auto hover:bg-violet-700">
-                        Continuar
+                        Continue
                         <ArrowRight size={20} className="text-violet-200" />
                     </button>
                 </form>
             </div>
             <p className="text-sm text-zinc-400">
-                *Só será possível recomendar jogos se a sua conta tiver visibilidade <span className="text-violet-500">pública</span>.
+                *It will only be possible to recommend games if your account has  
+                <a href="https://help.steampowered.com/en/faqs/view/588C-C67D-0251-C276" className="text-violet-500 underline hover:text-violet-400 mx-1">
+                    public
+                </a> 
+                visibility.
             </p>
         </div>
     )
--- front-end/app/(home)/page.tsx
+++ front-end/app/(home)/page.tsx
@@ -1,11 +1,11 @@
-import UserSteamForm from "./_components/UserSteamForm";
+import UserSteamForm from "./_components/userSteamForm";
 
 export default function Home() {
   return (
     <main className="container mx-auto h-screen flex flex-col justify-center items-center text-center gap-5 bg-pattern bg-no-repeat bg-center">
       <div className="">
         <h1 className="text-5xl text-violet-500 font-bold">Steam-IA</h1>
-        <h2 className="text-xl font-semibold text-violet-300">Recomendador de jogos</h2>
+        <h2 className="text-xl font-semibold text-violet-300">Game Recommender</h2>
       </div>
       <UserSteamForm />
     </main>
